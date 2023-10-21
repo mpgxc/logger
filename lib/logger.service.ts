f3c27540d93eac84794019f9214c375440caa642
mpgxc <mpgx5.c@gmail.com>
2023-10-21 18:04:15 -0300

    feat: add default name to logger instance when not provided
    this is useful when create a instance be like `new LoggerService("provider_context_name")`

--- lib/logger.service.ts
+++ lib/logger.service.ts
@@ -1,10 +1,10 @@
 import { ConsoleLogger, Injectable, Scope } from '@nestjs/common';
 
 @Injectable({
-  scope: Scope.REQUEST,
+  scope: Scope.TRANSIENT,
 })
 export class LoggerService extends ConsoleLogger {
   constructor(context?: string) {
-    super(context);
+    super(context ?? 'LoggerService');
   }
 }
